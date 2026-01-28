30994d1d643f1b74bbf615f51d546b9005a76e8f
Ryan Läuppi <pa33xxl@eduvaud.ch>
2026-01-28 13:02:05 +0100

    feat(route):ajout d'une route pour voir les détails des enseignants
    [10][Done]

--- app-teachers/app/controllers/teachers_controller.ts
+++ app-teachers/app/controllers/teachers_controller.ts
@@ -26,10 +26,6 @@ export default class TeachersController {
   /**
    * Show individual record
    */
-  async show({ params }: HttpContext) {}
-  /**
-   * Edit individual record
-   */
   async edit({ params }: HttpContext) {}
   /**
    * Handle form submission for the edit action
@@ -38,5 +34,26 @@ export default class TeachersController {
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {}
+  async destroy({ params, session, response }: HttpContext) {
+    // Sélectionne l'enseignant à supprimer
+    const teacher = await Teacher.findOrFail(params.id)
+    // Supprime l'enseignant
+    await teacher.delete()
+    // Afficher un message à l'utilisateur
+    session.flash(
+      'success',
+      `L'enseignant ${teacher.lastname} ${teacher.firstname} a été supprimé avec succès !`
+    )
+    // Redirige l'utilisateur sur la home
+    return response.redirect().toRoute('home')
+  }
+  /**
+   * Afficher les détails d'un enseignant (y compris le nom de sa section)
+   */
+  async show({ params, view }: HttpContext) {
+    // Sélectionner l'enseignant dont on veut afficher les détails
+    const teacher = await Teacher.query().where('id', params.id).preload('section').firstOrFail()
+    // Afficher la vue
+    return view.render('pages/teachers/show.edge', { title: "Détail d'un enseignant", teacher })
+  }
 }
--- app-teachers/start/routes.ts
+++ app-teachers/start/routes.ts
@@ -9,3 +9,6 @@
 import TeachersController from '#controllers/teachers_controller'
 import router from '@adonisjs/core/services/router'
 router.get('/', [TeachersController, 'index']).as('home')
+router.get('/teacher/:id/show', [TeachersController, 'show']).as('teacher.show')
+// Route permettant de supprimer un enseignant
+router.delete('/teacher/:id/destroy', [TeachersController, 'destroy']).as('teacher.destroy')
