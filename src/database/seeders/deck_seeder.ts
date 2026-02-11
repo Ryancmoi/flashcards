eb336a7fb2c870462825757b055bc0a6df916197
Ryan Läuppi <pa33xxl@eduvaud.ch>
2026-02-11 14:22:35 +0100

    feat(seed de la db): j'ai fais les seeders de cards et de deck en 1 seul fichier deck_seeders et seeder la db
    [15][Done]

--- src/database/seeders/deck_seeder.ts
+++ src/database/seeders/deck_seeder.ts
@@ -1,7 +1,26 @@
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import Deck from '#models/deck'
+import Card from '#models/card'
 
 export default class extends BaseSeeder {
   async run() {
-    // Write your database queries inside the run method
+    const deckMaths = await Deck.create({ title: 'Maths', description: 'Calculs' })
+    await Card.createMany([
+      {
+        question: '2-2',
+        answer: '0',
+        deckId: deckMaths.id,
+      },
+      {
+        question: '4x2',
+        answer: '8',
+        deckId: deckMaths.id,
+      },
+      {
+        question: '10 + 10',
+        answer: '20',
+        deckId: deckMaths.id,
+      },
+    ])
   }
 }
